0504cceb46559b140f5afe80031a8b21127cf5e2
ahjf-07 <sun.jian.kdev@gmail.com>
2026-01-27 10:35:12 +0800

    Simplify -K/-T forwarding in auto-bpf-ci

--- sj-ktools/auto-bpf-ci.sh
+++ sj-ktools/auto-bpf-ci.sh
@@ -401,12 +401,10 @@ fi
   if [ "$force_incremental" -eq 1 ]; then
     bargs="$bargs -i"
   fi
-  if [ "$NO_BUILD" -eq 0 ]; then
-    if [ "$KERNEL_ONLY" -eq 1 ] && [ "$TESTS_ONLY" -eq 0 ]; then
-      bargs="$bargs -K"
-    elif [ "$TESTS_ONLY" -eq 1 ] && [ "$KERNEL_ONLY" -eq 0 ]; then
-      bargs="$bargs -T"
-    fi
+  if [ "$KERNEL_ONLY" -eq 1 ] && [ "$TESTS_ONLY" -eq 0 ]; then
+    bargs="$bargs -K"
+  elif [ "$TESTS_ONLY" -eq 1 ] && [ "$KERNEL_ONLY" -eq 0 ]; then
+    bargs="$bargs -T"
   fi
   bargs="$bargs -r \"$LINUX_ROOT\" -o \"$O\""
   run "\"$TOOL_DIR/build-bpf.sh\" $bargs |& tee \"$RUN_DIR/build.all.log\""
